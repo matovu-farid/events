99117f67996dcaf06a50a3a9343af63edbab9c57
matovu-farid <matovu90@gmail.com>
2022-07-05 17:49:05 +0300

    Add icons to the buttons

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,15 +1,18 @@
 import Button from "../ui/Button";
-import styles from './event-item.module.css';
+import styles from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = ({ event }) => {
   const { title, image, date, location, id } = event;
-  const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  })
-  const formattedAddress = location.replace(', ','\n')
-  const exploreLink = `/events/${id}`
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
   return (
     <li className={styles.item}>
       <img src={`/${image}`} alt={title} />
@@ -17,14 +20,21 @@ const EventItem = ({ event }) => {
         <div className={styles.summary}>
           <h2>{title}</h2>
           <div className={styles.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={styles.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={styles.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,15 +1,14 @@
 import Link from "next/link";
 import React from "react";
-import styles from './button.module.css'
+import styles from "./button.module.css";
 
 const Button = ({ children, link }) => {
-  return <div className={styles.btn}>
-  <Link  href={link}>
-
-      {children}
-    </Link>
-    </div>
-    ;
+  return (
+      <Link href={link}>
+        <a className={styles.btn}>{children}</a>
+      </Link>
+    
+  );
 };
 
 export default Button;
